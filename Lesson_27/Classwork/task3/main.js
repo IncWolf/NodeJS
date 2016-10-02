2b301211d30a712c06e3ad8c15cbccfe4deccc02
IncWolf <incwolf@gmail.com>
2016-10-03 01:42:35 +0300

    init cw

---
+++ Lesson_27/Classwork/task3/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by IncWolf on 03.10.2016.
+ */
+const mod1 = require('./test/test.js');
+const mod2 = require('./test');
+const mod3 = require('test');
+
+mod1();
+mod2();
+mod3();
---
+++ Lesson_27/Classwork/task3/node_modules/test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by IncWolf on 03.10.2016.
+ */
+module.exports = function () {
+    console.log('test module loaded');
+}
