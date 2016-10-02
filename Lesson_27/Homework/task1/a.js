6c75bd9a7c34028414bcdfbff628e43e2cf94dbf
IncWolf <incwolf@gmail.com>
2016-10-03 01:50:53 +0300

    init cw

---
+++ Lesson_27/Homework/task1/a.js
@@ -0,0 +1,13 @@
+/**
+ * Created by IncWolf on 03.10.2016.
+ */
+var a = 10;
+var b = function (x) {
+    console.log(x);
+}
+
+
+module.exports = {
+    a: a,
+    b: b
+}
---
+++ Lesson_27/Homework/task1/b.js
@@ -0,0 +1,6 @@
+/**
+ * Created by IncWolf on 03.10.2016.
+ */
+var loaded_module = require('./A');
+console.log(loaded_module.a);
+console.log(loaded_module.b(loaded_module.a));
