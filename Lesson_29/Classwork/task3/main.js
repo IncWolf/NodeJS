e01ffc36ebbb1a1f57c6c5caa98500f98e697ca7
IncWolf <incwolf@gmail.com>
2016-10-05 00:45:03 +0300

    lesson 29

--- Lesson_29/Classwork/task3/main.js
+++ Lesson_29/Classwork/task3/main.js
@@ -7,6 +7,13 @@ const port = process.env.port || 1337;
 const server = http.createServer((req, res) => {
 
     console.log(` ${req.method} \n\r ${req.url} `);
+
+    if (req.url == '/test') {
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(JSON.stringify('{"message": "Hello World!"}'));
+        res.end();
+    }
 });
 server.listen(port, function() {
     console.log('server running on port ' + port);
